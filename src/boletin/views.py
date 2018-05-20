d3e6566cd261caf691aa2fd30209b3887e587e7d
Cristian Galnares Lopez <crisgalop@hotmail.es>
2018-05-20 16:01:33 +0200

    video 14
    validaciones de formularios

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .forms import RegForm
 
 def inicio(request):
-	form = RegForm()
+	form = RegForm(request.POST or None)
+	if form.is_valid():
+		form_data = form.cleaned_data
+		print form.cleaned_data.get("nombre")
+		print form.cleaned_data.get("edad")
 	context = {
 		"el_form": form,
 	}

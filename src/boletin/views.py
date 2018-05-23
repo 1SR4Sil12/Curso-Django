8bf6ce7882243e660b9fdd93546e5fa349c5b2cd
Cristian Galnares Lopez <crisgalop@hotmail.es>
2018-05-24 00:08:31 +0200

    video 39 final
    querysets

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -38,7 +38,7 @@ def inicio(request):
 	#	obj = Registrado.objects.create(email=abc, nombre=abc2)
 	
 	if request.user.is_authenticated() and request.user.is_staff:
-		queryset = Registrado.objects.all()
+		queryset = Registrado.objects.all().order_by("-timestamp")#.filter(nombre__icontains="a")
 		context ={
 			"queryset": queryset,
 		}
